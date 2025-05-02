8806c3b3065fe9cbf447f0a86d58097dc2fb5eb2
geonheey <ghyjas0704@gmail.com>
2025-05-02 09:11:14 +0900

    feat: 디버그 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
 import 'screens/movenet_app.dart';
 
-void main() => runApp(MaterialApp(home: MoveNetApp()));
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MoveNetApp(),
+    );
+  }
+}
